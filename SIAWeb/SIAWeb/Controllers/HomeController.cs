d3b2690662d36228cf024e8507712f933967a6e9
Dave820SA <dwdavidson@live.com>
2017-03-30 23:41:57 -0500

    end of day 3/30

--- SIAWeb/SIAWeb/Controllers/HomeController.cs
+++ SIAWeb/SIAWeb/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SIABusinessLayer;
+using SIAUserBusinessLayer;
+using SIAWeb.Models;
 
 namespace SIAWeb.Controllers
 {
@@ -11,6 +13,29 @@ namespace SIAWeb.Controllers
     {
         public ActionResult Index()
         {
+            SIAUserEntities user = new SIAUserEntities();
+            List<CurrentUser> currentUser = new List<CurrentUser>();
+           
+            var myUser = from u in user.spGetSIAWebUserInfo("dd94223")
+                       select u;
+            
+                foreach (var u in myUser)
+                {
+                    CurrentUser theUser = new CurrentUser();
+                    theUser.AppEntityID = Convert.ToInt32(u.AppEntityID);
+                    theUser.PIN = u.PIN.ToString();
+                    theUser.SAP = u.SAP.ToString();
+
+                    theUser.UserName = u.UserName.ToString();
+                    theUser.DutyStatus = u.DutyStatus.ToString();
+                    theUser.OfficeCode = u.OfficeCode.ToString();
+
+                    currentUser.Add(theUser);
+                }
+                ViewData["TheUser"] = currentUser;
+          
+           
+
             WebLinksBusinessLayer weblinkBusinessLayer = new WebLinksBusinessLayer();
             List<SIAWebLinks> siaWebLinks = weblinkBusinessLayer.SIAWebLinks.ToList();
             return View(siaWebLinks);
---
+++ SIAWeb/SIAWeb/Models/CurrentUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SIAWeb.Models
+{
+    public class CurrentUser
+    {
+        public int AppEntityID { get; set; }
+        public string PIN { get; set; }
+        public string SAP { get; set; }
+        public string UserName { get; set; }
+        public string DutyStatus { get; set; }
+        public string OfficeCode { get; set; }
+    }
+}
