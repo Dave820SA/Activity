60306d1c6abb2ccfa11cadec70f9214267f18371
Dave820SA <dwdavidson@live.com>
2017-04-02 23:32:54 -0500

    End of day4/2

---
+++ SIAWeb/SIAWebLinksBusinessLayer/WebLinksMetadata.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+	
+
+namespace SIAWebLinksBusinessLayer
+{
+    [MetadataType(typeof(WebCategories.Metadata))]
+    public partial class WebCategories
+    {
+       sealed class Metadata
+        {
+            [Key]
+            public int WebCategoriesID { get; set; }
+            [Required]
+            [Display(Name="Web Category")]
+            public string Name { get; set; }
+        }
+    }
+
+    [MetadataType(typeof(WebLinks.Metadata))]
+    public partial class WebLinks
+    {
+        sealed class Metadata
+        {
+            [Key]
+            public int WebLinkID { get; set; }
+
+            [Required]
+            [Display(Name = "Web Category")]
+            public string WebCategoryID { get; set; }
+
+            [Required]
+            [Display(Name = "Web Name")]
+            public string Name { get; set; }
+
+            [Required]
+            [Display(Name = "Web Address")]
+            public string WebLink { get; set; }
+
+            [Display(Name= "Visaible")]
+            public Boolean VisibleFlag { get; set; }
+        }
+    }
+}
