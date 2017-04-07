55af50413f728ead28294dbda7ebc0aa92424448
Dave820SA <dwdavidson@live.com>
2017-04-07 13:20:00 -0500

    End of day 4/7

---
+++ SIAWeb/SOPWeb/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SOPWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to the SOP Web App.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your Administrator Page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "SOP Contacts.";
+
+            return View();
+        }
+    }
+}
