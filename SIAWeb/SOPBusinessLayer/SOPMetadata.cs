9ed40740e8cc543c2cc982f89e527664e25e86d9
Dave820SA <dwdavidson@live.com>
2017-04-16 23:25:56 -0500

    End of weekend 4/16

--- SIAWeb/SOPBusinessLayer/SOPMetadata.cs
+++ SIAWeb/SOPBusinessLayer/SOPMetadata.cs
@@ -28,20 +28,20 @@ namespace SOPBusinessLayer
         }
     }
 
-    //[MetadataType(typeof(Bureau.Metadata))]
-    //public partial class Bureau
-    //{
-    //    sealed class Metadata
-    //    {
-    //        [Key]
-    //        public int BureauID { get; set; }
+    [MetadataType(typeof(Bureau.Metadata))]
+    public partial class Bureau
+    {
+        sealed class Metadata
+        {
+            [Key]
+            public int BureauID { get; set; }
 
-    //        [Required]
-    //        [Display(Name = "Bureau")]
-    //        public String Name { get; set; }
+            [Required]
+            [Display(Name = "Bureau")]
+            public String Name { get; set; }
 
-    //    }
-    //}
+        }
+    }
 
     [MetadataType(typeof(SOP_vCurrentDoc.Metadata))]
     public partial class SOP_vCurrentDoc
@@ -51,9 +51,15 @@ namespace SOPBusinessLayer
             [Key]
             public int ID { get; set; }
 
-            [Required]
+            //[Required]
             [Display(Name = "Bureau")]
             public String Bureau { get; set; }
+
+            [Display(Name = "SOP")]
+            public string SOP { get; set; }
+
+            [Display(Name = "Efective On")]
+            public DateTime StartDate { get; set; }
 
         }
     }
