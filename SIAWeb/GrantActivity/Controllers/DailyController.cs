b756cb22a10a505ec93742a0c55df51a2090fa09
Dave820SA <dwdavidson@live.com>
2017-05-22 14:23:41 -0500

    End of day5/22

--- SIAWeb/GrantActivity/Controllers/DailyController.cs
+++ SIAWeb/GrantActivity/Controllers/DailyController.cs
@@ -24,9 +24,48 @@ namespace GrantActivity.Controllers
             var daily = from d in db.Grant_Daily
                         select d;
 
-            daily = daily.Where(d => d.AppEntityID == userId);
+            daily = daily.Where(d => d.AppEntityID == userId && d.ApprovedFlag == false);
 
             return View(daily.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Index(string Answer)
+        {
+            int userId = Convert.ToInt32(System.Web.HttpContext.Current.Session["AppEntityID"]);
+
+            var dailyactivities = from d in db.Grant_Daily.Where(d => d.AppEntityID == userId)
+                                  select d;
+            switch (Answer)
+            {
+                case "Last30":
+                    var baseline = DateTime.Now.AddDays(-30);
+                    dailyactivities = dailyactivities.Where(d => d.AppEntityID == userId && d.EnteredDate >= baseline)
+                        .OrderByDescending(date => date.EnteredDate);
+                    break;
+                case "Approve":
+                    dailyactivities = dailyactivities.Where(d => d.AppEntityID == userId && d.ApprovedFlag == true)
+                        .OrderByDescending(date => date.EnteredDate);
+                    break;
+                case "NotApprove":
+                    dailyactivities = dailyactivities.Where(d => d.AppEntityID == userId && d.ApprovedFlag == false)
+                        .OrderByDescending(date => date.EnteredDate);
+                    break;
+                case "MoreInfo":
+                    dailyactivities = dailyactivities.Where(d => d.AppEntityID == userId && d.MoreInformationFlag == true)
+                        .OrderByDescending(date => date.EnteredDate);
+                    break;
+                case "All":
+                    dailyactivities = dailyactivities.Where(d => d.AppEntityID == userId)
+                        .OrderByDescending(date => date.EnteredDate);
+                    break;
+                default:
+                    dailyactivities = dailyactivities.Where(d => d.AppEntityID == userId)
+                        .OrderByDescending(date => date.EnteredDate);
+                    break;
+            }
+
+            return View(dailyactivities.ToList());
         }
 
         //
