2a7f1f6e68b055e1c5af2edbdbdacfe6c98f7bd4
Dave820SA <dwdavidson@live.com>
2017-05-22 22:56:29 -0500

    End of night 5/22

---
+++ SIAWeb/GrantActivity/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GrantActivity.Models;
+
+namespace GrantActivity.Controllers
+{
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GiveReview(RatingAndReviews model)
+        {
+            model.InsertRating(model.UserId, model.RatingStar, model.ConsumerName, model.ReviewHeader, model.ReviewContent);
+            return View("SubmittedReview", model);
+        }
+    }
+}
---
+++ SIAWeb/GrantActivity/Models/Rating.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GrantActivity.Models
+{
+    public class RatingAndReviews
+    {
+        public int UserId { get; set; }
+        public int RatingStar { get; set; }
+        public string ConsumerName { get; set; }
+        public string ReviewHeader { get; set; }
+        public string ReviewContent { get; set; }
+    }
+}
