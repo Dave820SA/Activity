4695c9746c63eb9f0426c3c8eb8e2dc26cd29340
Dave820SA <dwdavidson@live.com>
2017-06-06 15:12:57 -0500

    End of day 6/6

--- SIAWeb/GrantActivity/Global.asax.cs
+++ SIAWeb/GrantActivity/Global.asax.cs
@@ -25,8 +25,8 @@ namespace GrantActivity
         protected void Session_Start()
         {
             SessionLogin user = new SessionLogin();
-            //string myUser = HttpContext.Current.User.Identity.Name.ToString();
-            string myUser = "dd94224";
+            string myUser = HttpContext.Current.User.Identity.Name.ToString();
+            //string myUser = "dd94224";
             user.getUserPin(myUser);
 
         }
